ce6dca9f5c87ae5f3dfb07c577b0bbbd87f0b086
ar-santos <arthursantos4367@gmail.com>
2022-12-27 19:18:14 -0300

    names atualizados html

--- src/js/pegarDados.js
+++ src/js/pegarDados.js
@@ -1,7 +1,7 @@
 let informAddress = document.getElementById('inform-address')
 let informSobre = document.getElementById('inform-sobre')
 let informCompany = document.getElementById('inform-company')
-let nameUser = document.getElementById('titulo')
+let nameUser = document.getElementById('nomeUser')
 
 import utils from './exibirEscondeCard.js'
 import getUsers from './request.js'
@@ -10,7 +10,7 @@ import helpers from './helpers.js'
 const getUserData = async () => {
 
     try {
-        let resultadoDigitado = document.getElementById("input-pesquisa").value
+        let resultadoDigitado = document.getElementById("input-pesquisarUser").value
         let userData = await getUsers(`https://jsonplaceholder.typicode.com/users/${resultadoDigitado}`)
 
         let usuario = userData
